e37079efa32250d8d1e2a51748407b69b3e7a257
Sonika Kumar <sonikak004@gmail.com>
2026-08-11 18:45:54 +0530

    Switch to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext'
@@ -10,10 +10,10 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter basename="/bookly">
+      <HashRouter>
         <App />
         <Toaster position="top-right" />
-      </BrowserRouter>
+      </HashRouter>
     </AppProvider>
   </React.StrictMode>,
 )
